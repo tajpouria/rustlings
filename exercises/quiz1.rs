93f9bb814a1ffe60ddcdcc8ecfb5ab7c23b94409
tajpouria <tajpouria.dev@gmail.com>
2023-09-30 17:38:48 +0200

    feat: add code changes for primitive types and a quiz exercise
    - Adds missing code for boolean variables in primitive_types1.rs.
    - Adds missing code for character variable in primitive_types2.rs.
    - Fixes the code to create an array with initial values in primitive_types3.rs.
    - Fixes the code to create a slice from an array in primitive_types4.rs.
    - Fixes the code to destructure a tuple in primitive_types5.rs.
    - Fixes the code to access an element from a tuple using indexing in primitive_types6.rs.
    - Adds a new function to calculate the price of apples based on a given price in quiz1.rs.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(price: i32) -> i32 {
+    if price >= 41 {
+        return price;
+    }
+
+    return price * 2;
+}
 
 // Don't modify this function!
 #[test]
